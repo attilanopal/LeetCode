28fc3267fab20c5ff46011d0e44613ca242e0f83
Muhammad Attila An Naufal <71872233+attilanopal@users.noreply.github.com>
2023-07-25 13:16:49 +0800

    Time: 2173 ms (35.93%), Space: 0B (100.00%) - LeetHub

---
+++ 1280-students-and-examinations/1280-students-and-examinations.sql
@@ -0,0 +1,13 @@
+SELECT st.student_id, st.student_name, su.subject_name, count(ex.subject_name) as attended_exams
+FROM
+  Students st
+  CROSS JOIN
+  Subjects su
+  LEFT JOIN
+  Examinations ex
+  ON
+    st.student_id = ex.student_id AND
+    su.subject_name = ex.subject_name
+GROUP BY st.student_id, su.subject_name
+ORDER BY st.student_id, su.subject_name ASC
+;
