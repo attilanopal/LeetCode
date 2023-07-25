c6758fb2783f96da37f3436f52e67aba00d8668c
Muhammad Attila An Naufal <71872233+attilanopal@users.noreply.github.com>
2023-07-25 11:15:26 +0800

    Time: 943 ms (51.22%), Space: 0B (100.00%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,5 +1,6 @@
 SELECT pres.id FROM Weather pres
   JOIN Weather past ON 
-    DATEDIFF(pres.recordDate, past.recordDate) = 1 AND
+    DATEDIFF(pres.recordDate, past.recordDate) = 1
+  WHERE
     pres.temperature > past.temperature
 ;
