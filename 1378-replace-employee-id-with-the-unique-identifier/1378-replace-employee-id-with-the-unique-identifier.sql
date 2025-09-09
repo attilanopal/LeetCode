473ba74c93e68b1c8c1abc3f399a96cd3701c43e
Muhammad Attila An Naufal <71872233+attilanopal@users.noreply.github.com>
2025-09-09 08:27:38 +0800

    1378-replace-employee-id-with-the-unique-identifier Easy
    Date solved: 09-09-2025

--- 1378-replace-employee-id-with-the-unique-identifier/1378-replace-employee-id-with-the-unique-identifier.sql
+++ 1378-replace-employee-id-with-the-unique-identifier/1378-replace-employee-id-with-the-unique-identifier.sql
@@ -1,4 +1,4 @@
-SELECT a.unique_id, b.name FROM EmployeeUNI a 
-RIGHT JOIN Employees b 
-  ON a.id = b.id
-;
+-- Write your PostgreSQL query statement below
+SELECT A.unique_id, B.name FROM EmployeeUNI A
+RIGHT JOIN Employees B USING(ID)
+ORDER BY A.unique_id;
