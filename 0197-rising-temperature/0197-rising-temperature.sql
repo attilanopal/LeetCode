d960ae4949b09338259e2a7f04c6000b558bcb69
Muhammad Attila An Naufal <71872233+attilanopal@users.noreply.github.com>
2025-09-09 10:42:52 +0800

    0197-rising-temperature Easy
    Date solved: 09-09-2025

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,6 +1,4 @@
-SELECT pres.id FROM Weather pres
-  JOIN Weather past ON 
-    DATEDIFF(pres.recordDate, past.recordDate) = 1
-  WHERE
-    pres.temperature > past.temperature
-;
+-- Write your PostgreSQL query statement below
+SELECT A.id FROM Weather A
+JOIN WEATHER B ON A.recordDate = B.recordDate + 1
+WHERE B.temperature < A.temperature;
