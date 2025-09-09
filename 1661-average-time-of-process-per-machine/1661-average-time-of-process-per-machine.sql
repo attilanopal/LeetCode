e70284d5639023d4112177e443ce39b96483726b
Muhammad Attila An Naufal <71872233+attilanopal@users.noreply.github.com>
2025-09-09 11:57:38 +0800

    1661-average-time-of-process-per-machine Easy
    Date solved: 09-09-2025

--- 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
+++ 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -1,13 +1,6 @@
-SELECT st.machine_id, ROUND(AVG(en.timestamp - st.timestamp),3) as processing_time
-FROM
-  Activity st
-  JOIN
-  Activity en
-  ON
-    en.activity_type = 'end' AND
-    st.activity_type = 'start' AND
-    en.machine_id = st.machine_id AND
-    en.process_id = st.process_id
+-- Write your PostgreSQL query statement below
+SELECT st.machine_id, ROUND(AVG(en.timestamp-st.timestamp)::NUMERIC,3) AS processing_time 
+    FROM Activity st
+JOIN Activity en ON en.machine_id = st.machine_id 
+    AND en.activity_type = 'end' AND st.activity_type = 'start'
 GROUP BY st.machine_id
-ORDER BY machine_id ASC
-;
