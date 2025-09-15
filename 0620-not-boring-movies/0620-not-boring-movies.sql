f43d0de3fe4b674fc9ed0d284fe0cd833ef8c960
Muhammad Attila An Naufal <71872233+attilanopal@users.noreply.github.com>
2025-09-15 22:50:10 +0800

    0620-not-boring-movies Easy
    Date solved: 09-15-2025

--- 0620-not-boring-movies/0620-not-boring-movies.sql
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -1,6 +1,4 @@
-SELECT * FROM Cinema 
-WHERE
-  description NOT LIKE '%boring%' AND 
-  (id % 2) != 0
+-- Write your PostgreSQL query statement below
+SELECT id, movie, description, rating FROM CINEMA
+WHERE ID % 2 != 0 AND description NOT LIKE '%boring%'
 ORDER BY rating DESC
-;
